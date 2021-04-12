677fc95a8835112b9dbdc80ffe7369d6e349739d
guptaji <jaisriram@gmail.com>
2021-04-12 16:48:07 +0530

    Generating QR code

---
+++ qrapp/src/main/java/com/example/qrapp/QrappApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrappApplication.class, args);
+	}
+
+}
---
+++ qrapp/src/test/java/com/example/qrapp/QrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
